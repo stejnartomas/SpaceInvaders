1a142f94e392c63f10f64319583d6271f765fc99
stejnartomas <t.stejnar@seznam.cz>
2025-04-02 19:46:42 +0200

    Changed main.js

--- res/js/main.js
+++ res/js/main.js
@@ -1,4 +1,4 @@
-class Player {
+class Player1 {
     constructor(x, y, w, h, c, v) {
         this.x = x;
         this.y = y;
@@ -6,6 +6,8 @@ class Player {
         this.h = h;
         this.c = c;
         this.v = v;
+        this.basicV = v;
+        this.maxV = v * 2;
     }
 
     draw(ctx) {
@@ -13,14 +15,36 @@ class Player {
         ctx.fillRect(this.x, this.y, this.w, this.h);
     }
     update(keys) {
-        if (keys["w"]) this.y -= this.v;
-        if (keys["s"]) this.y += this.v;
-        if (keys["a"]) this.x -= this.v;
-        if (keys["d"]) this.x += this.v;
-
+        if (keys["w"]) {
+            if (this.y - this.v > 0) {
+                this.y -= this.v;
+            }
+        }
+        if (keys["s"]) {
+            if (this.y + this.h + this.v < canvas.height) {
+            this.y += this.v;
+            }
+        }
+        if (keys["a"]) {
+            if (this.x - this.v > 0) {
+                this.x -= this.v;
+            }
+        }
+        if (keys["d"]) {
+            if (this.x + this.w + this.v < canvas.width) {
+                this.x += this.v;
+            }
+        }
+        if (keys[" "]) this.w-- && this.h--;
+        if (keys["k"]) {
+            this.v = this.maxV;
+        } else {
+            this.v = this.basicV;
+        }
+        if (keys["b"]) this.w++ && this.h++;
     }
 }
-const myPlayer = new Player(10, 10, 50, 50, "cyan", 5);
+const myPlayer1 = new Player1(10, 10, 50, 50, "cyan", 5);
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -37,11 +61,31 @@ let keys = {};
 window.addEventListener("keydown", (e) => keys[e.key] = true);
 window.addEventListener("keyup", (e) => keys[e.key] = false);
 
+const testObject = {
+    w: 100,
+    h: 100,
+    x: 200,
+    y: 300,
+    c: "blue"
+}
+
 const gameLoop = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    myPlayer.update(keys);
-    myPlayer.draw(ctx);
-    
+    myPlayer1.update(keys);
+    myPlayer1.draw(ctx);
+    ctx.fillStyle = testObject.c;
+    ctx.fillRect (testObject.x, testObject.y, testObject.w, testObject.h)
+    checkCollision(myPlayer1, testObject);
     requestAnimationFrame(gameLoop);
 }
 requestAnimationFrame(gameLoop);
+
+const checkCollision = (object1, object2) => {
+    if (object1.x + object1.w > object1.x &&
+        object1.x < object2.x + object2.w &&
+        object1
+    ) {
+
+           
+    }
+}
